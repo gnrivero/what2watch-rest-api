93da5ff3bfb355e69b7dade7eb0629d67a81566e
maxculen86 <mxculen@gmail.com>
2020-06-17 05:51:50 -0300

    Agregar logica de emoticones con valores

--- src/main/java/com/what2watch/restapplication/controller/ContentController.java
+++ src/main/java/com/what2watch/restapplication/controller/ContentController.java
@@ -3,11 +3,19 @@ package com.what2watch.restapplication.controller;
 import com.what2watch.restapplication.model.Content;
 import com.what2watch.restapplication.repository.specifications.content.ContentSpecificationBuilder;
 import com.what2watch.restapplication.service.ContentService;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -24,8 +32,8 @@ public class ContentController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Content>> getContent(@RequestParam(value = "search") String search){
-
+    public ResponseEntity<List<Content>> getContent(@RequestParam(value = "search") String search) throws IOException, ParseException {
+        search = replaceEmotionData(search);
         ContentSpecificationBuilder builder = new ContentSpecificationBuilder();
         Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
         Matcher matcher = pattern.matcher(search + ",");
@@ -37,9 +45,102 @@ public class ContentController {
         return new ResponseEntity(this.contentService.findAll(spec),HttpStatus.OK);
     }
 
+    private String replaceEmotionData(String search) throws IOException, ParseException {
+        //type:serie,anger:2,disgust:1,sad:1,happy:10
+        String[] emoIds = findEmotionId(search);
+        JSONArray emoData = getJsonArrayEmotionalData();
+        ArrayList<JSONArray> matchList = addListMatches(emoIds, emoData);
+        JSONArray blendResult = getBlendJson(matchList);
+        JSONObject nonEmptyValuesResult = removeEmptyValues(blendResult.getJSONObject(0));
+        String emotionText = formatString(nonEmptyValuesResult.toString());
+        return search.substring(0,search.indexOf(",")) + "," + emotionText;
+    }
+
+    private String formatString(String emotionText) {
+        emotionText = emotionText.replaceAll("[{}]", "");
+        emotionText = emotionText.replaceAll("\"", "");
+        return emotionText;
+    }
+
+    private JSONObject removeEmptyValues(JSONObject jsonObject) {
+        JSONObject validValues = new JSONObject();
+        for(String key : jsonObject.keySet()){
+            if(jsonObject.getInt(key) != 0){
+                validValues.put(key,jsonObject.getInt(key));
+            }
+        }
+        return validValues;
+    }
+
+    private JSONArray getBlendJson(ArrayList<JSONArray> matchList){
+        JSONArray result = new JSONArray();
+        for (JSONArray element : matchList){
+            if(result.isEmpty()) {
+                result.put(element.get(0));
+            } else {
+                mergeResult(element, result);
+            }
+        }
+        return result;
+    }
+
+    private void mergeResult(JSONArray element, JSONArray result) {
+        JSONObject elementObject = element.getJSONObject(0);
+        JSONObject resultObject = result.getJSONObject(0);
+        for (Object key : elementObject.keySet()) {
+            String keyName = (String)key;
+            int keyValue = elementObject.getInt(keyName);
+            int combinedValued = combineValues(resultObject.getInt(keyName),keyValue);
+            resultObject.put(keyName,combinedValued);
+        }
+        for (int i=0; i<result.length(); i++) {
+            result.remove(i);
+        }
+
+        result.put(resultObject);
+    }
+
+    private int combineValues(int o, int keyValue) {
+        if (o >= keyValue ){
+            return o;
+        } else {
+            return keyValue;
+        }
+    }
+
+    private ArrayList<JSONArray> addListMatches(String[] emoIds, JSONArray emoData) {
+        ArrayList<JSONArray> resultList = new ArrayList<>();
+        for(String id : emoIds) {
+            findEmoVaules(emoData, id, resultList);
+        }
+        return resultList;
+    }
+
+    private void findEmoVaules(JSONArray emoData, String emoId, ArrayList<JSONArray> resultList) {
+        JSONObject temp;
+        for (int i=0; i<emoData.length(); i++){
+            temp = (JSONObject) emoData.get(i);
+            if (temp.get("id").equals(emoId)) {
+                resultList.add((JSONArray) temp.get("emotionsDetails"));
+            };
+        }
+    }
+
+    private JSONArray getJsonArrayEmotionalData() throws IOException, ParseException {
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonEmoData = new JSONObject(jsonParser.parse(new FileReader("src/main/emotionalConfig.json")).toString());
+        return (JSONArray) jsonEmoData.get("emotionData");
+    }
+
+    private String[] findEmotionId(String search) {
+        String [] stringArray = search.substring(search.indexOf("emoid=")).split(";|=");
+        return Arrays.copyOfRange(stringArray, 1, stringArray.length);
+    }
+
     @PostMapping
     public ResponseEntity<?> createContent(@RequestBody Content content){
         contentService.save(content);
         return new ResponseEntity<Content>(content, HttpStatus.CREATED);
     }
+
 }
